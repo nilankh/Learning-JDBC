ec91436f6fef26330b065b3194a081c7003fe245
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-02-02 13:22:01 +0530

    learning jdbc

--- DemoClass.java
+++ DemoClass.java
@@ -13,12 +13,22 @@ import java.sql.*;
 public class DemoClass {
 
 	public static void main(String[] args) throws Exception{
-		String url = "";
-		String uname = "";
+		String url = "jdbc:mysql://localhost:3306/aliens";//3306 port number for first tiem & 3307 for second time
+		String uname = "root";
 		String pass = "";
+		String query = "";
 		
 		Class.forName("com.mysql.jdbc.Driver");//Class forname telusko u can watch
 		Connection con = DriverManager.getConnection(url, uname, pass);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		
+		rs.next();
+		String name = rs.getString("username");
+		System.out.println(name);
+		
+		st.close();
+		con.close();
 	}
 
 }
